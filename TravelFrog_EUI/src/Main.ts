f2cc616e66e32e040af5b0961f38a64bb09f7b01
15708444034 <690717014@qq.com>
2018-01-31 18:37:51 +0800

    场景切换

--- TravelFrog_EUI/src/Main.ts
+++ TravelFrog_EUI/src/Main.ts
@@ -123,19 +123,13 @@ class Main extends eui.UILayer {
      */
 	private courtyard: Courtyard;
     private room: Room;
-    
+     
     protected createGameScene(): void {
 
-	  Data.initlist();
+	    Data.initlist();
         this.courtyard = new Courtyard();
         this.addChild(this.courtyard);
 
-   
-    
-        //this.room=new Room(); 
-        //this.addChild(this.room);
-
-   
         let icon = new egret.Bitmap();
         icon.texture = RES.getRes("icon_house_84_88_png");
         this.addChild(icon);
@@ -143,11 +137,13 @@ class Main extends eui.UILayer {
         icon.y = Data.getscreenHeight() - icon.height - 10;
         icon.touchEnabled=true;
         icon.addEventListener(egret.TouchEvent.TOUCH_BEGIN,this.loadRoom,this);
+
         let clover = new egret.Bitmap();
         clover.texture = RES.getRes("sys_clover_window_png");
         this.addChild(clover);
         clover.x = 15;
         clover.y = 10;
+
         let clovertext = new egret.TextField;
         clovertext.text = "113";
         clovertext.textColor = 0;
@@ -162,6 +158,39 @@ class Main extends eui.UILayer {
         this.removeChild(this.courtyard);
         this.room=new Room(); 
         this.addChild(this.room);
+         let icon = new egret.Bitmap();
+        icon.texture = RES.getRes("icon_house_84_88_png");
+        this.addChild(icon);
+        icon.x = Data.getscreenWidth() - icon.width - 15;
+        icon.y = Data.getscreenHeight() - icon.height - 10;
+        icon.touchEnabled=true;
+        icon.addEventListener(egret.TouchEvent.TOUCH_BEGIN,this.loadCourtyard,this);
+
+    }
+    private loadCourtyard(){
+        this.courtyard = new Courtyard();
+        this.addChild(this.courtyard);
+
+        let icon = new egret.Bitmap();
+        icon.texture = RES.getRes("icon_house_84_88_png");
+        this.addChild(icon);
+        icon.x = Data.getscreenWidth() - icon.width - 15;
+        icon.y = Data.getscreenHeight() - icon.height - 10;
+        icon.touchEnabled=true;
+        icon.addEventListener(egret.TouchEvent.TOUCH_BEGIN,this.loadRoom,this);
+
+        let clover = new egret.Bitmap();
+        clover.texture = RES.getRes("sys_clover_window_png");
+        this.addChild(clover);
+        clover.x = 15;
+        clover.y = 10;
+
+        let clovertext = new egret.TextField;
+        clovertext.text = "113";
+        clovertext.textColor = 0;
+        clovertext.x = 150;
+        clovertext.y = 30;
+        this.addChild(clovertext);
     }
     
 }
