14dab1664432311a6d38370b86ac309b94573c15
15708444034 <690717014@qq.com>
2018-02-02 19:14:31 +0800

    Main

--- TravelFrog_EUI/src/Main.ts
+++ TravelFrog_EUI/src/Main.ts
@@ -121,15 +121,12 @@ class Main extends eui.UILayer {
      * 创建场景界面
      * Create scene interface
      */
-<<<<<<< HEAD
-	
-     private control:Control;
-=======
+
 	private courtyard: Courtyard;
     private room: Room;
     private shop_EUI:Shop_EUI;
      
->>>>>>> d467a24380802dbe0d0b3e4a30743067cfd84f6b
+
     protected createGameScene(): void {
 
         Data.readShopTable();
@@ -137,36 +134,12 @@ class Main extends eui.UILayer {
         this.addChild(this.control);
 	   
 
-<<<<<<< HEAD
-=======
+
         this.shop_EUI=new Shop_EUI();
         this.addChild(this.shop_EUI);
 
-	    Data.initlist();
-        this.courtyard = new Courtyard();
-        //this.addChild(this.courtyard);
+	  
 
-        let icon = new egret.Bitmap();
-        icon.texture = RES.getRes("icon_house_84_88_png");
-        this.addChild(icon);
-        icon.x = Data.getscreenWidth() - icon.width - 15;
-        icon.y = Data.getscreenHeight() - icon.height - 10;
-        icon.touchEnabled=true;
-        icon.addEventListener(egret.TouchEvent.TOUCH_BEGIN,this.loadRoom,this);
-
-        let clover = new egret.Bitmap();
-        clover.texture = RES.getRes("sys_clover_window_png");
-        this.addChild(clover);
-        clover.x = 15;
-        clover.y = 10;
-
-        let clovertext = new egret.TextField;
-        clovertext.text = "113";
-        clovertext.textColor = 0;
-        clovertext.x = 150;
-        clovertext.y = 30;
-        this.addChild(clovertext);
->>>>>>> d467a24380802dbe0d0b3e4a30743067cfd84f6b
        
         
 
